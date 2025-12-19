71d6681f85020f8696a06932b761f2c323a54cc7
upshawam <134899406+upshawam@users.noreply.github.com>
2025-12-18 23:40:51 -0600

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Running_Calendar/",
+  base: "/Running_Calendar-dev/",
   build: {
     outDir: 'docs',
     emptyOutDir: false
