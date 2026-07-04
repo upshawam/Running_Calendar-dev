00ae9b036300f0ea208c7c0cd9c4eaf16f0b539a
upshawam <134899406+upshawam@users.noreply.github.com>
2026-07-04 17:42:02 -0500

    Refactor WeekSummary component by removing modified plan logic and related state

--- src/components/WeekSummary.tsx
+++ src/components/WeekSummary.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { renderDist, getWeekDistance } from "../ch/rendering";
 import StartIcon from "../svg/icons02/start.svg";
 import FinishIcon from "../svg/icons02/finish.svg";
 import HighMileageIcon from "../svg/highMileage.svg";
 import { RacePlan } from "../ch/dategrid";
 import { Week, DayDetails, Units } from "types/app";
-import { hasModifiedWeek, getModifiedWeek } from "../data/modifiedPlans";
-import { ModifiedPlanPopout } from "./ModifiedPlanPopout";
 
 interface Props {
   desc: string;
@@ -31,39 +29,12 @@ export const WeekSummary = ({
   planId,
 }: Props) => {
   const distance = getWeekDistance(week, units);
-  const [showModifiedPlan, setShowModifiedPlan] = useState(false);
-  
-  // Check if this week has a modified plan for this user
-  const hasModified = hasModifiedWeek(selectedUser, planId, week.weekNum);
-  const modifiedWeek = hasModified ? getModifiedWeek(selectedUser, planId, week.weekNum) : undefined;
-  
-  const handleModifiedClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setShowModifiedPlan(true);
-  };
   
   return (
     <>
       <div className="week-summary" key={"week:" + week.weekNum}>
         <p>
           <strong>{`Week ${week.weekNum}`}</strong>
-          {hasModified && (
-            <>
-              {" "}
-              <a
-                href="#"
-                onClick={handleModifiedClick}
-                style={{
-                  fontSize: "0.85rem",
-                  color: "#4a90e2",
-                  textDecoration: "none",
-                  fontWeight: "normal",
-                }}
-              >
-                modified
-              </a>
-            </>
-          )}
         </p>
         {distance[0] > 0 && <p>{renderDist(distance, units, units)}</p>}
         {isFirstWeek && <img src={StartIcon} alt={"Start"} />}
@@ -75,12 +46,6 @@ export const WeekSummary = ({
           </p>
         )}
       </div>
-      {showModifiedPlan && modifiedWeek && (
-        <ModifiedPlanPopout
-          weekPlan={modifiedWeek}
-          onClose={() => setShowModifiedPlan(false)}
-        />
-      )}
     </>
   );
 };
